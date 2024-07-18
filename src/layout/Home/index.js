792fd68543ea34891f48266ea5e00cc137614b3a
Do Tuan Minh <tmminhdt@gmail.com>
2024-07-18 20:23:19 +0700

    fix

--- src/layout/Home/index.js
+++ src/layout/Home/index.js
@@ -7,80 +7,78 @@ import logo from "../../image/logo vuông.png";
 import { Link } from "react-router-dom";
 import { IoMdCloseCircleOutline } from "react-icons/io";
 
-function Home(){
+function Home() {
+    const [showModal, setShowModal] = useState(true); // State to control modal visibility
+    const [showModalIntro, setShowModalIntro] = useState(false); // State to control intro modal visibility
+    const modalRef = useRef(null); // Ref for modal
 
-    const [showModal, setShowModal] = useState(true); // State để kiểm soát hiển thị modal
-    const [showModalIntro, setShowModalIntro] = useState(false); // State để kiểm soát hiển thị modal
-    const modalRef = useRef(null); // Ref cho modal
-
-    const[minh, setMinh] = useState(true);
-    setMinh(false);
+    const [minh, setMinh] = useState(true);
 
     var today = new Date();
-    var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+    var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
     console.log(date);
 
     const [data, setData] = useState(null);
 
     const modalContentRef = useRef(null);
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`https://google-doodle-v2-v2.vercel.app/api/v1/doodle/detail/661a4816d1818c2981bee5d5`)
-            .then(res=>res.json())
+            .then(res => res.json())
             .then(db => {
                 setData(db);
-            })
-    },[minh])
+            });
+    }, [minh]);
 
-    const [dataNewese, setDataNewese] = useState([]);
-    useEffect(()=>{
+    const [dataNewest, setDataNewest] = useState([]);
+    useEffect(() => {
         fetch(`https://google-doodle-v2-v2.vercel.app/api/v1/doodle/newest`)
-            .then(res=>res.json())
+            .then(res => res.json())
             .then(data => {
-                setDataNewese(data.slice(0,9));
-            })
-    },[])
+                setDataNewest(data.slice(0, 9));
+            });
+    }, []);
 
     const [dataNear, setDataNear] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`https://google-doodle-v2-v2.vercel.app/api/v1/doodle/upcoming`)
-            .then(res=>res.json())
+            .then(res => res.json())
             .then(data => {
-                setDataNear(data.slice(0,3));
-            })
-    },[])
+                setDataNear(data.slice(0, 3));
+            });
+    }, []);
 
     const [dataPopular, setDataPopular] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`https://google-doodle-v2-v2.vercel.app/api/v1/doodle/popular`)
-            .then(res=>res.json())
+            .then(res => res.json())
             .then(data => {
-                setDataPopular(data.slice(0,9));
-            })
-    },[])
+                setDataPopular(data.slice(0, 9));
+            });
+    }, []);
 
     const handleClickOutside = (event) => {
-        // Kiểm tra xem click có xảy ra bên ngoài modal không
+        // Check if click occurred outside the modal
         if (modalRef.current && !modalRef.current.contains(event.target)) {
-            setShowModal(false); // Nếu click bên ngoài modal, đóng modal
+            setShowModal(false); // Close modal if click is outside
             setShowModalIntro(false);
         }
     };
 
     const handleEditClick = () => {
-        setShowModal(true); // Khi nhấn vào nút "Sửa", hiển thị modal
-        setShowModalIntro(true); // Khi nhấn vào nút "Sửa", hiển thị modal
+        setShowModal(true); // Show modal when edit button is clicked
+        setShowModalIntro(true); // Show intro modal when edit button is clicked
     }
 
-    const introButton = () =>{
+    const introButton = () => {
         setShowModalIntro(true);
         setShowModal(false);
     }
-    
-    return(
+
+    return (
         <>
             {data && (
                 <Link to={"/" + data.slug} className="sectionPic">
-                    <div className="sectionPic__image" style={{backgroundImage: `url(${data.image})`}}></div>
+                    <div className="sectionPic__image" style={{ backgroundImage: `url(${data.image})` }}></div>
                     <div className="sectionPic__box">
                         <div className="sectionPic__box__title">
                             {data.title}
@@ -109,28 +107,28 @@ function Home(){
                     </div>
                 </div>
                 <div className="sectionDisc__site2">
-                    <img src={Thumbnail} alt="Thumbnail"/>
+                    <img src={Thumbnail} alt="Thumbnail" />
                 </div>
             </div>
-            <Line/>
+            <Line />
             <div className="sectionNew">
                 <div className="sectionNew__title">
                     Sự kiên sắp diễn ra
                 </div>
                 <div className="sectionNew__box">
-                    {dataNear.map(item=>(
-                            <Link to={"/" + item.slug} className="sectionNew__box__innerBox" key={item._id}>
-                                <div className="sectionNew__box__innerBox__image" style={{backgroundImage: `url(${item.image})`}}></div>
-                                <div className="sectionNew__box__innerBox__line"></div>
-                                <div className="sectionNew__box__innerBox__date">{item.time.dateString}</div>
-                                <div className="sectionNew__box__innerBox__title">{item.title}</div>
-                            </Link>
+                    {dataNear.map(item => (
+                        <Link to={"/" + item.slug} className="sectionNew__box__innerBox" key={item._id}>
+                            <div className="sectionNew__box__innerBox__image" style={{ backgroundImage: `url(${item.image})` }}></div>
+                            <div className="sectionNew__box__innerBox__line"></div>
+                            <div className="sectionNew__box__innerBox__date">{item.time.dateString}</div>
+                            <div className="sectionNew__box__innerBox__title">{item.title}</div>
+                        </Link>
                     ))}
                 </div>
             </div>
             <div className="sectionHow">
                 <div className="sectionHow__pic">
-                    <img src={howPic} alt="Cach dung"/>
+                    <img src={howPic} alt="Cach dung" />
                 </div>
                 <div className="sectionHow__text">
                     <div className="sectionHow__text__title">
@@ -162,13 +160,13 @@ function Home(){
                     Doodle mới
                 </div>
                 <div className="sectionNew__box">
-                    {dataNewese.map(item=>(
-                            <Link to={"/" + item.slug} className="sectionNew__box__innerBox" key={item._id}>
-                                <div className="sectionNew__box__innerBox__image" style={{backgroundImage: `url(${item.image})`}}></div>
-                                <div className="sectionNew__box__innerBox__line"></div>
-                                <div className="sectionNew__box__innerBox__date">{item.time.dateString}</div>
-                                <div className="sectionNew__box__innerBox__title">{item.title}</div>
-                            </Link>
+                    {dataNewest.map(item => (
+                        <Link to={"/" + item.slug} className="sectionNew__box__innerBox" key={item._id}>
+                            <div className="sectionNew__box__innerBox__image" style={{ backgroundImage: `url(${item.image})` }}></div>
+                            <div className="sectionNew__box__innerBox__line"></div>
+                            <div className="sectionNew__box__innerBox__date">{item.time.dateString}</div>
+                            <div className="sectionNew__box__innerBox__title">{item.title}</div>
+                        </Link>
                     ))}
                 </div>
             </div>
@@ -177,13 +175,13 @@ function Home(){
                     Doodle thịnh hành
                 </div>
                 <div className="sectionNew__box">
-                    {dataPopular.map(item=>(
-                            <Link to={"/" + item.slug} className="sectionNew__box__innerBox" key={item._id}>
-                                <div className="sectionNew__box__innerBox__image" style={{backgroundImage: `url(${item.image})`}}></div>
-                                <div className="sectionNew__box__innerBox__line"></div>
-                                <div className="sectionNew__box__innerBox__date">{item.time.dateString}</div>
-                                <div className="sectionNew__box__innerBox__title">{item.title}</div>
-                            </Link>
+                    {dataPopular.map(item => (
+                        <Link to={"/" + item.slug} className="sectionNew__box__innerBox" key={item._id}>
+                            <div className="sectionNew__box__innerBox__image" style={{ backgroundImage: `url(${item.image})` }}></div>
+                            <div className="sectionNew__box__innerBox__line"></div>
+                            <div className="sectionNew__box__innerBox__date">{item.time.dateString}</div>
+                            <div className="sectionNew__box__innerBox__title">{item.title}</div>
+                        </Link>
                     ))}
                 </div>
             </div>
@@ -194,7 +192,7 @@ function Home(){
                     </div>
                 </Link>
             </div>
-            <Line/>
+            <Line />
             <div className="sectionCert">
                 <div className="sectionCert__title">
                     Sản phẩm của chúng tôi an toàn
@@ -203,7 +201,7 @@ function Home(){
                     Sản phẩm này đã vượt qua bài kiểm tra về sự an toàn do các bên có thẩm quyền
                 </div>
                 <div className="sectionCert__detail">
-                    Đảm bảo rằng VietDoodle Gallery là một phần mềm sạch 100%. Sản phẩm phần mềm này đã được kiểm tra kỹ lưỡng và được đánh giá là hoàn toàn sạch sẽ; do đó, nó có thể được cài đặt mà không cần quan tâm đến bất kỳ vấn đề bảo mật dự liệu nào
+                    Đảm bảo rằng VietDoodle Gallery là một phần mềm sạch 100%. Sản phẩm phần mềm này đã được kiểm tra kỹ lưỡng và được đánh giá là hoàn toàn sạch sẽ; do đó, nó có thể được cài đặt mà không cần quan tâm đến bất kỳ vấn đề bảo mật dữ liệu nào
                 </div>
             </div>
 
@@ -214,7 +212,7 @@ function Home(){
                 <div className="home__modal" ref={modalRef}>
                     <div className='home__modal--buttonClose' onClick={() => setShowModal(false)}><IoMdCloseCircleOutline /></div>
                     <div className="home__modal--content" ref={modalContentRef}>
-                        <img src={logo} className='home__modal--content__img'/>
+                        <img src={logo} className='home__modal--content__img' />
                         <div className='home__modal--content__intro'>
                             <div className='home__modal--content__intro__text'>
                                 Chưa biết cách sử dụng?
@@ -274,7 +272,7 @@ function Home(){
                 </div>
             )}
         </>
-    )
+    );
 }
 
 export default Home;
